ba7e99d5689c74eb3f61db532260d3861515627f
RazDV <raz.david.2002@gmail.com>
2025-11-02 19:57:08 +0200

    First try

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,28 @@
 // Computes the future value of a saving investment.
+// futureValue = currentValue ⋅ (1 + rate)^n
+// currentValue, which is the invested sum, (not negative integer)
+// The annual interest rate is assumed to be a percentage
+// The number of years n is assumed to be a nonnegative integer.
+
+// % java FVCalc 100 10 2
+// After 2 years, $100 saved at 10.0% will yield $121
+
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		String currentValueString = args[0];
+		String rateString = args[1];
+		String nString = args[2];
+		
+		
+		double rate = Double.parseDouble(rateString) / 100;
+		double n = Double.parseDouble(nString);
+		double currentValue = Double.parseDouble(currentValueString);
+
+		double futureValue = currentValue * Math.pow((1 + rate), n);
+
+		System.out.println("After " + (int) n + " years, $" + (int) currentValue + " saved at " + rate * 100 +
+						   "% will yield $" + (int) futureValue);
+		
+
 	}
 }
